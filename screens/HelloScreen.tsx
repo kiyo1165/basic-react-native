7eff02458319e48c270cb4d626b7fc0bcab2eb20
akashi <kiyotsugu.send@gmail.com>
2022-06-22 19:22:26 +0900

    create useMemo useCallback

--- screens/HelloScreen.tsx
+++ screens/HelloScreen.tsx
@@ -1,19 +1,26 @@
 import { View, Text } from 'react-native';
-import React, { VFC, useEffect } from 'react';
+import React, { VFC, useEffect, useState, useCallback } from 'react';
 import tw from 'tailwind-rn';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types/types';
-import { Button } from 'react-native-elements';
+import { Button, Input } from 'react-native-elements';
+import { Child } from '../components/Child';
+import { SafeAreaInsetsContext } from 'react-native-safe-area-context';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Hello'>;
 
 export const HelloScreen: VFC<Props> = ({ navigation }) => {
+  const [text, setText] = useState('');
+  const [printText, setPrintText] = useState('');
   useEffect(() => {
     console.log('mounted Hello');
     return () => {
       console.log('un mounted Hello');
     };
   }, []);
+  const printMsg = useCallback(() => {
+    console.log(`print: ${printText} `);
+  }, [printText]);
   return (
     <View style={tw('flex-1 bg-gray-300 justify-center items-center')}>
       <Text>HeloScreen</Text>
@@ -23,6 +30,20 @@ export const HelloScreen: VFC<Props> = ({ navigation }) => {
           onPress={() => navigation.navigate('ReduxTK')}
         />
       </View>
+      <Input
+        placeholder='"type print text'
+        autoCompleteType={undefined}
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+      <Input
+        placeholder='"type print Msg'
+        autoCompleteType={undefined}
+        value={printText}
+        onChangeText={(text) => setPrintText(text)}
+      />
+      <Text>{printText}</Text>
+      <Child printMsg={printMsg} />
     </View>
   );
 };
